582c7236287aad23d94802c626631629b1718b20
maxim_koryakov <maxim_koryakov@bitbucket.org>
2020-03-04 12:16:05 +0300

    добавил событие по клику на кнопку и ответ на сколько делить пикселей картинку

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,24 @@ document.addEventListener("DOMContentLoaded", function() {
   let width = document.querySelector(".calculate__width").value;
   let height = document.querySelector(".calculate__height").value;
   let result = document.querySelector(".calculate__result");
-  Number(width);
-  Number(height);
-
-  const calculate__buttons = addEventListener("click", function() {
-    const formula = Math.round(Math.sqrt(29420 / (1366 / 1.7)));
-    result.innerHTML; // соответственно не работает))
-    console.log(formula);
+  let calculate__button = document.querySelector(".calculate__button");
+  
+  calculate__button.addEventListener("click", function() {
+    // if (width == "" &&  height == "") {
+    //   return console.log("errror, введён текст")
+    // }
+    const formula = Math.round(Math.sqrt(Number(height) / (Number(width) / 1.7)));
+    const answer = Math.round(Number( height) / formula )
+    result.innerHTML = answer
   });
 });
+
+
+// где будем округлять ? под корнем или когда считаем, на сколько частей поделить ?
+
+// Сделать проверку на буквы и прочее непотребство
+// Добавить событие по клику на enter
+// После ответа на событие очищать поля ввода
+// И забывать результат, когда заново кликаешь, чтобы выдавал новый
+
+
