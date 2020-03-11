f87503505ade70465edeba479005b4c215a5ad45
minytian <minytian@yandex.ru>
2020-03-11 23:21:27 +0300

    Добавил стили Bootstrap. Их надо удалить или добавить из библиотеки node_module. Сделаны инпуты и чтение их значений. Добавлен развёрнутый ответ.

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let width = document.querySelector(".calculate__width").value;
-  let height = document.querySelector(".calculate__height").value;
+document.addEventListener("DOMContentLoaded", function () {
+  let width = document.querySelector(".calculate__width");
+  let height = document.querySelector(".calculate__height");
   let result = document.querySelector(".calculate__result");
   let calculate__button = document.querySelector(".calculate__button");
-  
-  calculate__button.addEventListener("click", function() {
-    // if (width == "" &&  height == "") {
-    //   return console.log("errror, введён текст")
-    // }
-    const formula = Math.round(Math.sqrt(Number(height) / (Number(width) / 1.7)));
-    const answer = Math.round(Number( height) / formula )
-    result.innerHTML = answer
+
+  calculate__button.addEventListener('click', function (event) {
+    let widthValue = width.value;
+    let heightValue = height.value;
+
+    const formula = Math.sqrt(Number(heightValue) / (Number(widthValue) / 1.7));
+
+    const answer = Number(heightValue) / formula;
+    result.innerHTML = `Поделить на ${formula.toFixed(2)} частей по ${answer.toFixed(2)}px`;
+
+    width.value = "";
+    height.value = "";
   });
 });
-
-
-// где будем округлять ? под корнем или когда считаем, на сколько частей поделить ?
 
 // Сделать проверку на буквы и прочее непотребство
 // Добавить событие по клику на enter
 // После ответа на событие очищать поля ввода
 // И забывать результат, когда заново кликаешь, чтобы выдавал новый
-
-
