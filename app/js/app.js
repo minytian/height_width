0507869955c7813c84446078976fbe0ba4a6a0a7
minytian <minytian@yandex.ru>
2020-03-24 23:06:35 +0300

    Сделана форма расчёта. Убранbootstrap cdn, но добавлен из библиотеки. Удален node_modules. Исправлен js.

--- app/js/app.js
+++ app/js/app.js
@@ -1,24 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let form = document.querySelector("form");
   let width = document.querySelector(".calculate__width");
   let height = document.querySelector(".calculate__height");
   let result = document.querySelector(".calculate__result");
   let calculate__button = document.querySelector(".calculate__button");
 
-  calculate__button.addEventListener('click', function (event) {
-    let widthValue = width.value;
-    let heightValue = height.value;
+  calculate__button.addEventListener('click', clickHandler)
+  form.addEventListener('keydown', clickHandler)
 
-    const formula = Math.sqrt(Number(heightValue) / (Number(widthValue) / 1.7));
 
-    const answer = Number(heightValue) / formula;
-    result.innerHTML = `Поделить на ${formula.toFixed(2)} частей по ${answer.toFixed(2)}px`;
 
-    width.value = "";
-    height.value = "";
-  });
+  function clickHandler(event) {
+    if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+      // Разрешаем: Ctrl+A
+      (event.keyCode == 65 && event.ctrlKey === true) ||
+      // Разрешаем: home, end, влево, вправо
+      (event.keyCode >= 35 && event.keyCode <= 39)) {
+      // Ничего не делаем
+      return;
+    }
+
+    if (event.key === "Enter" || event.which == 1) {
+      console.log(event)
+
+      let widthValue = width.value;
+      let heightValue = height.value;
+
+      const formula = Math.sqrt(Number(heightValue) / (Number(widthValue) / 1.7));
+
+      const answer = Number(heightValue) / formula;
+      result.innerHTML = `Поделить на ${formula.toFixed(1)} частей по ${answer.toFixed(2)}px`;
+
+      width.value = "";
+      height.value = "";
+    }
+    // Запрещаем все, кроме цифр на основной клавиатуре, а так же Num-клавиатуре
+    else {
+      if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+        event.preventDefault();
+      }
+
+    };
+  }
 });
 
+// Сделано !
+// создать две функции клткхендлер на клик и кейап
 // Сделать проверку на буквы и прочее непотребство
 // Добавить событие по клику на enter
 // После ответа на событие очищать поля ввода
 // И забывать результат, когда заново кликаешь, чтобы выдавал новый
+
+
+
+// Не сделано
+// Разрешить кнопкам лево право таб бэкспасе делет работать. код ниже
+
+// if (event.key === "Enter" || event.which == 1) {
+    // Разрешаем: backspace, delete, tab
+    // if ((event.keyCode < 46 || event.keyCode == 8 || event.keyCode == 9) ||
+    //   // Разрешаем: home, end, влево, вправо
+    //   (event.keyCode >= 35 && event.keyCode <= 39)) {
+    //   return
+    // }
